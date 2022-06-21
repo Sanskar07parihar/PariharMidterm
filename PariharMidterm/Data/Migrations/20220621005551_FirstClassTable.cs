7b71675084e2b3a1e52ee3019b78bf7d89b2a57d
Sanskar Parihar <200489011@student.georgianc.on.ca>
2022-06-20 20:57:35 -0400

    update

---
+++ PariharMidterm/Data/Migrations/20220621005551_FirstClassTable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PariharMidterm.Data.Migrations
+{
+    public partial class FirstClassTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Laptops",
+                columns: table => new
+                {
+                    LaptopId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Processor = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RAM = table.Column<int>(type: "int", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DeliveryDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Total = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Laptops", x => x.LaptopId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Laptops");
+        }
+    }
+}
