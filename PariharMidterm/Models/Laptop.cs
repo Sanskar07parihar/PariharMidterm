71f74080620e265d46f3803b1a72afd1b0e15bf8
Sanskar Parihar <200489011@student.georgianc.on.ca>
2022-06-20 20:32:37 -0400

    Add project files.

---
+++ PariharMidterm/Models/Laptop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PariharMidterm.Models
+{
+    public class Laptop
+    {
+        public int LaptopId { get; set; }
+        public string Processor { get; set; }
+        public int RAM { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public double Total { get; set; }
+    }
+}
